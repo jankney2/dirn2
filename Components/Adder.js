29cebabf6d81838257a4d5cb50d20497fb28862a
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2.app.byu.edu>
2019-09-09 08:37:36 -0600

    working on adder

--- Components/Adder.js
+++ Components/Adder.js
@@ -1,48 +1,102 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import {View, Button, Text, StyleSheet, TextInput, ScrollView, SafeAreaView} from 'react-native'
-
-
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
 
 export default class Adder extends Component {
-    state = { 
-        seller:'', 
-        sellerPhone:'', 
-        newListName:'', 
-        street:'', 
-        city:'', 
-        state:'', 
-        zip:'', 
-        bedrooms:'', 
-        price:''
-     }
-    render() {
-        return (
-            <SafeAreaView>
-                <ScrollView>
-                    <TextInput onChangeText={this.textChanger} />
-                </ScrollView>
-            </SafeAreaView>
-        
-        );
-    }
+  state = {
+    seller: '',
+    sellerPhone: '',
+    newListName: '',
+    street: '',
+    city: '',
+    state: '',
+    zip: '',
+    bedrooms: '',
+    price: '',
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+          <KeyboardAvoidingView>
+
+
+        <ScrollView>
+          <TextInput
+          placeholder='seller name (John Doe)'
+          style={styles.input}
+            onChangeText={text => {
+              this.setState({
+                seller: text,
+              });
+            }}
+          />
+          <TextInput
+          placeholder='seller phone'
+            keyboardType='number-pad'
+            style={styles.input}
+            onChangeText={text => {
+              this.setState({
+                sellerPhone: text,
+              });
+            }}
+          />
+          <TextInput
+            placeholder='list name'
+            style={styles.input}
+            onChangeText={text => {
+              this.setState({
+                newListName: text,
+              });
+            }}
+          />
+
+            <Text>{this.state.seller}</Text>
+
+          <TouchableOpacity>
+            <Button
+              title="add property!"
+              onPress={() => {
+                axios.post('https://dropin.business/api/addListIndividual');
+              }}
+            />
+          </TouchableOpacity>
+        </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    );
+  }
 }
 
-seller,
-      bathrooms,
-      newListName,
-      street,
-      city,
-      state,
-      zip,
-      bedrooms,
-      price, 
+// seller,
+//       bathrooms,
+//       newListName,
+//       street,
+//       city,
+//       state,
+//       zip,
+//       bedrooms,
+//       price,
 
-const styles=StyleSheet.create({
-    contain: {
-        flex:1, 
-        justifyContent:'space-evenly',
-        alignItems:'center' 
-
-    }
-})
+const styles = StyleSheet.create({
+  contain: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  input:{
+      borderBottomColor:'black', 
+      borderBottomWidth:2
+  }
+});
