804ba470165c3b6a3c1be0d8290899fcae8ceb61
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-09-25 13:46:03 -0600

    icons in nav bar

--- Components/TabNav.js
+++ Components/TabNav.js
@@ -1,40 +1,52 @@
+import React from 'react'
+
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation'
 import Settings from './Settings'
 import HomePage from './HomePage'
 import 
 AllProperties from './AllProperties'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import CrmList from './CrmList'
 import Adder from './Adder'
+const settingsIcon=<Icon name='settings' size={30} color={'black'} />
+const nearby=<Icon name='my-location' size={30} color={'black'} />
+const listIcon=<Icon name='format-list-bulleted' size={30} color={'black'} />
+const addIcon=<Icon name='add-circle-outline' size={30} color={'black'} />
+
 const small = createBottomTabNavigator(
     {
       settings: {
         screen: Settings,
         navigationOptions: {
           tabBarLabel: 'Settings',
+          tabBarIcon:settingsIcon
         },
       },
       homePage: {
         screen: HomePage,
         navigationOptions: {
           tabBarLabel: 'Nearby',
+          tabBarIcon:nearby
         },
       },
       properties: {
         screen: AllProperties,
         navigationOptions: {
           tabBarLabel: 'Properties',
+          tabBarIcon:listIcon
         },
       },
-      CRM: {
-        screen: CrmList,
-        navigationOptions: {
-          tabBarLabel: 'Export List',
-        },
-      },
+      // CRM: {
+      //   screen: CrmList,
+      //   navigationOptions: {
+      //     tabBarLabel: 'Export List',
+      //   },
+      // },
       adder: {
         screen:Adder, 
         navigationOptions:{
-          tabBarLabel:'Add'
+          tabBarLabel:'Add',
+          tabBarIcon:addIcon
         }
       }
     },
