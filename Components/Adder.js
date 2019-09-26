db57b0d59811156ef3d0a32b9e5f0d4afc9c9789
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2-010037206217.app.byu.edu>
2019-09-26 15:34:42 -0600

    bug with the individual adder

--- Components/Adder.js
+++ Components/Adder.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import {Button} from 'react-native-elements'
+import {Button} from 'react-native-elements';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   ScrollView,
+  Keyboard,
   KeyboardAvoidingView,
   SafeAreaView,
   TouchableOpacity,
 } from 'react-native';
-import axios from 'axios';
 
+import axios from 'axios';
 
 export default class Adder extends Component {
   state = {
@@ -29,143 +30,144 @@ export default class Adder extends Component {
   render() {
     return (
       <SafeAreaView>
-          <KeyboardAvoidingView>
-
-
-        <ScrollView>
-          <TextInput
-          placeholder='seller name (John Doe)'
-          style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                seller: text,
-              });
-            }}
-          />
-          <TextInput
-          placeholder='seller phone'
-            keyboardType='number-pad'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                sellerPhone: text,
-              });
-            }}
-          />
-          <TextInput
-          placeholder='seller email'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                email: text,
-              });
-            }}
-          />
-        
-          <TextInput
-            placeholder='street'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                street: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='city'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                city: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='state (UT, AZ, VA)'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                state: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='ZipCode'
-            style={styles.input}
-            keyboardType='number-pad'
-            onChangeText={text => {
-              this.setState({
-                zip: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='bedrooms'
-            style={styles.input}
-            keyboardType='number-pad'
-            onChangeText={text => {
-              this.setState({
-                bedrooms: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='bathrooms'
-            style={styles.input}
-            keyboardType='number-pad'
-            onChangeText={text => {
-              this.setState({
-                bathrooms: text,
-              });
-            }}
-          />
-          <TextInput
-            placeholder='Price'
-            keyboardType='number-pad'
-            style={styles.input}
-            onChangeText={text => {
-              this.setState({
-                price: text,
-              });
-            }}
-          />
-
-          <TouchableOpacity>
-            <Button
-              buttonStyle={{backgroundColor:'red'}}
-              title="add property!"
-              onPress={() => {
-//UNTESTED
-
-
-                Keyboard.dismiss()
-                axios.post('https://dropin.business/api/addListIndividual', {
-                seller:this.state.seller, 
-                bathrooms:this.state.bathrooms, 
-                street:this.state.street, 
-                city: this.state.city, 
-                zip:this.state.zip, 
-                state:this.state.state,
-                bedrooms:this.state.bedrooms, 
-                price:this.state.price,
-                phone:this.state.sellerPhone, 
-                email:this.state.email
-                }).then(res=>{
-                  this.props.navigation.navigate('properties')
+        <KeyboardAvoidingView>
+          <ScrollView>
+            <TextInput
+              placeholder="seller name (John Doe)"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  seller: text,
                 });
               }}
             />
-          </TouchableOpacity>
-        </ScrollView>
+            <TextInput
+              placeholder="seller phone"
+              keyboardType="number-pad"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  sellerPhone: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="seller email"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  email: text,
+                });
+              }}
+            />
 
+            <TextInput
+              placeholder="street"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  street: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="city"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  city: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="state (UT, AZ, VA)"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  state: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="ZipCode"
+              style={styles.input}
+              keyboardType="number-pad"
+              onChangeText={text => {
+                this.setState({
+                  zip: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="bedrooms"
+              style={styles.input}
+              keyboardType="number-pad"
+              onChangeText={text => {
+                this.setState({
+                  bedrooms: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="bathrooms"
+              style={styles.input}
+              keyboardType="number-pad"
+              onChangeText={text => {
+                this.setState({
+                  bathrooms: text,
+                });
+              }}
+            />
+            <TextInput
+              placeholder="Price"
+              keyboardType="number-pad"
+              style={styles.input}
+              onChangeText={text => {
+                this.setState({
+                  price: text,
+                });
+              }}
+            />
 
+            <TouchableOpacity>
+              <Button
+                buttonStyle={{backgroundColor: 'red'}}
+                title="add property!"
+                onPress={() => {
+                  //UNTESTED
+
+                  Keyboard.dismiss();
+                  axios
+                    .post('https://dropin.business/api/addListMobile', { 
+                      userId:14,
+                      properties:[
+                        {
+                          seller: this.state.seller,
+                          bathrooms: this.state.bathrooms,
+                          street: this.state.street,
+                          city: this.state.city,
+                          zip: this.state.zip,
+                          state: this.state.state,
+                          bedrooms: this.state.bedrooms,
+                          price: this.state.price,
+                          phone: this.state.sellerPhone,
+                          email: this.state.email,
+                        }
+                      ]
+                    })
+                    .then(res => {
+                      console.log(res)
+                      // this.props.navigation.navigate('properties');
+                    });
+                }}
+              />
+            </TouchableOpacity>
+          </ScrollView>
         </KeyboardAvoidingView>
       </SafeAreaView>
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   contain: {
@@ -173,9 +175,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
   },
-  input:{
-      borderBottomColor:'black', 
-      borderBottomWidth:2, 
-      padding:5
-  }
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    padding: 5,
+  },
 });
